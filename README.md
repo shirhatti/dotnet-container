92f5a5ebe9595809cef94ad2b46e6ce4802186e7
Sourabb Shirhatti <shirhatti@outlook.com>
2019-05-14 01:46:53 -0700

    Add script for generating SP

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dotnet-oci
 A .NET global tool for producing and publishing OCI images
+
+### Creating a Service Principal for authentication
+
+> Do **NOT** commit the file containing the Service Principal into source control.
+
+Modify the script in `scripts/generate-sp.sh` and replace `ACR_NAME` with the name of your registry.
+Generate a Service Principal (SP) by running the following command:
+
+```sh
+./scripts/generate-sp.sh > sp.azureauth
+```
+
+If you do not have the `az` cli installed locally, I recommend using the [Azure Cloud Shell](https://shell.azure.com/).
---
+++ scripts/generate-sp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Modify for your environment.
+# ACR_NAME: The name of your Azure Container Registry
+# SERVICE_PRINCIPAL_NAME: Must be unique within your AD tenant
+ACR_NAME=<container-registry-name>
+SERVICE_PRINCIPAL_NAME=acr-service-principal
+
+# Obtain the full registry ID for subsequent command args
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+
+# Create the service principal with rights scoped to the registry.
+# Default permissions are for docker pull access. Modify the '--role'
+# argument value as desired:
+# acrpull:     pull only
+# acrpush:     push and pull
+# owner:       push, pull, and assign roles
+az ad sp create-for-rbac --name http://$SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpush
