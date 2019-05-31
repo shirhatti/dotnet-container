7ad1aa8a97500a0bf2fce38aa17bb7b3f40171bd
Sourabh Shirhatti <shirhatti@outlook.com>
2019-05-30 23:41:37 -0700

    Add credential helper (on Windows)

---
+++ src/dotnet-container/CredentialHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Dotnet.Container
+{
+    internal static class CredentialHelper
+    {
+        private static readonly string _defaultRegistry = "https://index.docker.io/v1/";
+
+        public static bool TryGetCredentials(out NetworkCredential credential)
+        {
+            return TryGetCredentials(_defaultRegistry, out credential!);
+        }
+
+        public static bool TryGetCredentials(string registry, out NetworkCredential? credential)
+        {
+            credential = default;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (WindowsCredentials.CredRead(registry, WindowsCredentials.CRED_TYPE.GENERIC, 0, out var credPtr))
+                {
+                    var cred = (WindowsCredentials.CREDENTIAL)Marshal.PtrToStructure(credPtr, typeof(WindowsCredentials.CREDENTIAL));
+                    credential = new NetworkCredential(userName: cred.userName,
+                                                       password: Marshal.PtrToStringAnsi(cred.credentialBlob, cred.credentialBlobSize));
+                    return true;
+                }
+            }
+
+            // TODO: Retrieve credentials stored in OS X keychain
+
+            return false;
+        }
+    }
+}
